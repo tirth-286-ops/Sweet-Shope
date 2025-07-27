7ce5908e4113f935883f03a4821892a16db7bfa0
tirth-286-ops <tirthmehta2004@gmail.com>
2025-07-27 15:27:07 +0530

    Third commit

--- backend_sweet/backend_sweet/urls.py
+++ backend_sweet/backend_sweet/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
       path('api/', include('sweet_app.urls')),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
